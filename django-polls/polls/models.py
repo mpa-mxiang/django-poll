6cf41e8c22e0d254ae7beaf6a1466c21213040c8
Jiachun Xiang <18576811+mpa-mxiang@users.noreply.github.com>
2023-10-28 22:35:39 -0400

    autofix pylint errors

--- django-polls/polls/models.py
+++ django-polls/polls/models.py
@@ -4,10 +4,11 @@ from django.contrib import admin
 from django.db import models
 from django.utils import timezone
 
+
 class Question(models.Model):
     question_text = models.CharField(max_length=200)
     pub_date = models.DateTimeField("date published")
-    
+
     @admin.display(
         boolean=True,
         ordering="pub_date",
@@ -15,13 +16,16 @@ class Question(models.Model):
     )
     def __str__(self):
         return self.question_text
+
     def was_published_recently(self):
         now = timezone.now()
         return now - datetime.timedelta(days=1) <= self.pub_date <= now
+
 
 class Choice(models.Model):
     question = models.ForeignKey(Question, on_delete=models.CASCADE)
     choice_text = models.CharField(max_length=200)
     votes = models.IntegerField(default=0)
+
     def __str__(self):
         return self.choice_text
